b3792b513c225fa22aac8361453b5957e5f9d07e
Spiderpig86 <slim679975@gmail.com>
2018-11-04 02:07:29 -0500

    test(test_transform.py): Added base for text transform test

---
+++ tests/test_transform.py
@@ -0,0 +1,16 @@
+import unittest
+from funcs.transform import *
+
+class TransformTest(unittest.TestCase):
+
+    def test_lower(self):
+        pass
+
+    def test_upper(self):
+        pass
+
+    def test_reverse(self):
+        pass
+
+    def test_leet(self):
+        pass
