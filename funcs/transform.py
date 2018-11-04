a2ade4b5376950ca8836b1a27399f9f437dc3b13
Spiderpig86 <slim679975@gmail.com>
2018-11-04 01:52:55 -0500

    feat(transform.py): Added functions for generatng text transformations

--- funcs/transform.py
+++ funcs/transform.py
@@ -1,17 +1,41 @@
-class Transformations:
+import re
+class Transformation:
 
-    def __init__(self, upper, reverse, leet):
+    def __init__(self, lower, upper, reverse, leet):
+        self.lower = lower
         self.upper = upper
         self.reverse = reverse
         self.leet = leet
 
     def __repr__(self):
-        return 'Transformation=(upper={}, reverse={}, leet={})'.format(self.upper, self.reverse, self.leet)
+        return 'Transformation=(lower={}, upper={}, reverse={}, leet={})'.format(self.lower, self.upper, self.reverse, self.leet)
 
 """
 " Generates all variants for each given string (uppercase, reverse, and leet-speak)
 " :arg strings: string[]
-" :return: { string, Transformations }
+" :return: { string, Transformation }
 """
 def generate_transformations(strings):
-    pass
+    res = dict()
+
+    for string in strings:
+        lower = lower(string)
+        upper = upper(string)
+        reverse = string[::-1]
+        leet = generate_leet(string) 
+        res[string] = Transform(lower, upper, reverse, leet)
+    
+    return res
+
+"""
+" Creates leet encoded version of string
+" :arg string: String
+" :return: String
+"""
+def generate_leet(string):
+    leet = re.sub('[Aa]', '4', string)
+    leet = re.sub('[Ee]', '3', leet)
+    leet = re.sub('[Ll]', '1', leet)
+    leet = re.sub('[Tt]', '7', leet)
+    leet = re.sub('[oO]', '0', leet)
+    return leet
