f44bf22becf7e61bce290b300e76e0fd82813958
Johnny So <jso123450@gmail.com>
2018-11-30 23:48:25 -0500

    Added support for configs.

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from tests.test_transform import *
+from configs import configs
 
-test_transform = TransformTest()
-test_transform.test_upper()
-test_transform.test_lower()
-test_transform.test_reverse()
-test_transform.test_leet()
+if __name__ == "__main__":
+    # Change the value of test to change what is tested
+    test = "configs"
+    if test == "configs":
+        config = configs.config
+        for val in config:
+            print("%s : %s" % (val,config[val]))
+    elif test == "test_transform":
+        test_transform = TransformTest()
+        test_transform.test_upper()
+        test_transform.test_lower()
+        test_transform.test_reverse()
+        test_transform.test_leet()
